d48e80a41be499d57d8eaf0da2ee17366af0de6d
Fernando Zanchetta <fernandozanca94@gmail.com>
2021-03-15 00:15:01 -0300

    Create a simples ToDo list using vue 3 composition api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Todo App</h1>
+  <form @submit.prevent="addTodo()">
+    <label>
+      New ToDo
+    </label>
+    <input
+      v-model="newTodo"
+      autocomplete="flase"
+      name="newTodo"
+    />
+    <button>
+      Add ToDo
+    </button>
+  </form>
+  <h2>
+    ToDo List
+  </h2>
+  <ul>
+    <li
+      v-for="(todo, index) in todos"
+      :key="index">
+      <span
+        :class="{done: todo.done}"
+        @click="doneTodo(todo)">
+        {{todo.content}}
+      </span>
+      <button @click="removeTodo(index)">
+        Remove
+      </button>
+    </li>
+    <h4 v-if="todos.length === 0">
+      Empty list.
+    </h4>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    const newTodo = ref('')
+    const defaultData = [{
+      done: false,
+      content: 'Write a blog post'
+    }]
+    const todosData = JSON.parse(localStorage.getItem('todos')) || defaultData
+    const todos = ref(todosData)
+
+    function addTodo () {
+      if (newTodo.value) {
+        todos.value.push({
+          done: false,
+          content: newTodo.value
+        })
+        newTodo.value = ''
+      }
+      saveData()
+    }
+
+    function doneTodo (todo) {
+      todo.done = !todo.done
+      saveData()
+    }
+
+    function removeTodo (index) {
+      todos.value.splice(index, 1)
+      saveData()
+    }
+
+    function saveData () {
+      const storageData = JSON.stringify(todos.value)
+      localStorage.setItem('todos', storageData)
+    }
+
+    return {
+      todos,
+      newTodo,
+      addTodo,
+      doneTodo,
+      removeTodo,
+      saveData
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
